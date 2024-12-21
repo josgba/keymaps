327d81d1d9851d3d34be29956c094e721e17ccca
Joseph Barsness <joseph@Josephs-MacBook-Pro-2.local>
2024-12-20 23:24:42 -0800

    chore: move tab/enter

--- keyboards/sweep/keymaps/simple-mo/keymap.c
+++ keyboards/sweep/keymaps/simple-mo/keymap.c
@@ -34,14 +34,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NUM] = LAYOUT( /* [> NUM <] */
     KC_1,           KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_GRV,         KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-    KC_ENT,         KC_TAB,  KC_TRNS, KC_BTN1, KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS,
-                                      KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS 
+    KC_TRNS,        KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
+    KC_TRNS,        KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS,
+                                      KC_TAB,  KC_ENT,          KC_TRNS, KC_TRNS 
   ),
 
   [_SYMBOL] = LAYOUT( /* [> SYMBOL <] */
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_QUOT, KC_GRV,          KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     KC_TRNS, KC_VOLD, KC_VOLU, KC_MRWD, KC_MFFD,         KC_UNDS, KC_PLUS, KC_LPRN, KC_RPRN, KC_TRNS,
                                KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS
   )
